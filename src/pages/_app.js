e391cba65ad72d53511282e2e6b77382aecd9aef
surya chawan <suryachawan112200@gmail.com>
2025-10-21 22:58:33 +0530

    m

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,21 @@
+import { AppProps } from "next/app";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-import { useEffect } from "react";
-import { auth } from "/lib/firebase";
-import { AuthProvider } from "/contexts/AuthContext"; // Adjust the path
-import "/src/styles/globals.css";
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      // This ensures the auth state is updated, but the context handles it
-    });
-    return () => unsubscribe();
-  }, []);
-
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
